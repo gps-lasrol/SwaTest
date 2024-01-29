b60353e41a70de3df7cd71a747b1f28ec74f7f32
Lasse Rolstad <lasse.rolstad@glasspaper.no>
2024-01-29 19:45:01 +0100

    Added server info

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <ServerInfo />
   </div>
 </template>
---
+++ components/ServerInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-if="data">
+        <code v-bind="data"></code>
+    </div>
+</template>
+
+<script setup>
+
+    const {data} = useFetch("/api/info");
+
+    console.log(data);
+
+</script>
